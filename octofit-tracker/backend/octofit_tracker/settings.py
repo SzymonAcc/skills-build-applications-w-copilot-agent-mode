8ec289486e0c81d57f9a0ff99fdbce912664d0b3
SzymonAcc <szymon.zajdel@accenture.com>
2025-12-15 15:26:57 +0000

    Step 4: Setup Django REST Framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-t-q)jdq5jt=wkyiuajho_74%x!ye3ab7jhe#h=y7=h2u3(c+!e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
